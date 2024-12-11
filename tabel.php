bcb059cba2909b4689724d5297de609a343ac3be
Babisama1 <kelvinlonelyboy@gmail.com>
2024-12-12 03:30:37 +0800

    C_Buat Koneksi
    Mau coba buat koneksi dengan tabel.php dan koneksi.php

---
+++ tabel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Tabel</title>
+</head>
+<body>
+    <h1>Halaman</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>Id</th>
+            <th>Judul</th>
+            <th>Tanggal</th>
+            <th>Cek</th>
+         </tr>
+    </table>
+
+    
+</body>
+</html>
