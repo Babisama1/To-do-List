33155b86831bfc839e833a591757e2559829cf9b
Babisama1 <kelvinlonelyboy@gmail.com>
2024-12-12 04:52:29 +0800

    C_Buat tabel
    Membuat percobaan koneksi didalam tabel

--- tabel.php
+++ tabel.php
@@ -1,3 +1,28 @@
+<?php
+//koneksi ke database
+$kon = mysqli_connect("localhost:3307", "root", "","todolist");
+//mengambil data dari tabel todolist
+$result = mysqli_query($kon, "SELECT * FROM todolistdb");
+
+if ($result) {
+    echo mysqli_error($kon);
+    echo "Tidak ada masalah bung";
+    echo "<br>";
+}
+
+//ambil data (fetch) todolistdb dari object result
+// mysqli_fetch_row() //mengembalikan array numerik
+// mysqli_fetch_assoc() //mengembalikan array associative
+// mysqli_fetch_array() //mengembalikan keduanya
+// mysqli_fetch_object() //mengembalikan dengan ->
+
+// while ($todo = mysqli_fetch_assoc($result)){
+//     var_dump($todo);
+// }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,14 +31,28 @@
     <title>Halaman Tabel</title>
 </head>
 <body>
-    <h1>Halaman</h1>
+    <h1>Halaman Tabel</h1>
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>Id</th>
             <th>Judul</th>
-            <th>Tanggal</th>
-            <th>Cek</th>
-         </tr>
+            <th>Deskripsi</th>
+            <th>Jatuh Tempo</th>
+            <th>Status</th>
+        </tr>   
+
+        <?php $i = 1; ?>
+        <?php while($todo = mysqli_fetch_assoc($result)) : ?>
+        <tr>
+            <th><?= $i ?></th>
+            <th><?= $todo["judul"]?></th>
+            <th><?= $todo["deskripsi"] ?></th>
+            <th><?= $todo["jatuh_tempo"] ?></th>
+            <th><?= $todo["statuss"] ?></th>
+        </tr>
+        <?php $i++; ?>
+        <?php endwhile; ?>
+
     </table>
 
     
